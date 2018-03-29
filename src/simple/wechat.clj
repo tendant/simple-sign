d10d767a8456596789761a65b002290d32830d55
Yangye Zhu <yangye.zhu@wishlife.com>
2018-03-29 16:12:44 -0700

    fix bug in wechat decrypt

--- src/simple/wechat.clj
+++ src/simple/wechat.clj
@@ -4,7 +4,22 @@
             [buddy.core.hash :as hash]
             [buddy.core.codecs :as codecs]
             [buddy.core.codecs.base64 :as base64]
-            [buddy.core.crypto :as crypto]))
+            [buddy.core.crypto :as crypto]
+            [cheshire.core :as json])
+  (:import (java.nio.charset Charset)
+           (java.time ZonedDateTime ZoneId)))
+
+(def ^:private GMT (ZoneId/of "GMT"))
+(def ^:private US-ASCII (Charset/forName "US-ASCII"))
+(def ^:private UTF-8 (Charset/forName "UTF-8"))
+
+(defn string->utf-8
+  [str]
+  (. str getBytes UTF-8))
+
+(defn utf-8->string
+  [bytes]
+  (String. bytes UTF-8))
 
 (defn verify-signature [session-key raw-data signature]
   (= signature
@@ -12,18 +27,13 @@
          hash/sha1
          codecs/bytes->hex)))
 
-(comment
-  (let [raw-data "{\"nickName\":\"Band\",\"gender\":1,\"language\":\"zh_CN\",\"city\":\"Guangzhou\",\"province\":\"Guangdong\",\"country\":\"CN\",\"avatarUrl\":\"http://wx.qlogo.cn/mmopen/vi_32/1vZvI39NWFQ9XM4LtQpFrQJ1xlgZxx3w7bQxKARol6503Iuswjjn6nIGBiaycAjAtpujxyzYsrztuuICqIM5ibXQ/0\"}"
-        session-key "HyVFkGl5F5OQWJZZaNzBBg=="
-        signature "75e81ceda165f4ffa64f4068af58c64b8f54b88c"]
-    (verify-signature session-key raw-data signature)))
-
-
 (defn decrypt-data
   [session-key encrypted-data iv]
   (let [eng (crypto/block-cipher :aes :cbc)
         aeskey (base64/decode session-key)
         encrypted-data (base64/decode encrypted-data)
         iv (base64/decode iv)]
-    (crypto/decrypt-cbc eng encrypted-data aeskey iv) ; This is a buddy.core.crypto internal api, it might not be supported in future version.
+    (-> (crypto/decrypt-cbc eng encrypted-data aeskey iv)
+        (utf-8->string)
+        (json/parse-string)) ; This is a buddy.core.crypto internal api, it might not be supported in future version.
     ))
