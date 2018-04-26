38a900c66b83613e1c0908ef1f8e71b52631cebc
Lei Wang <tendant@gmail.com>
2018-04-26 16:13:36 -0700

    release 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-sign "0.1.2"
+(defproject simple-sign "0.1.3"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
