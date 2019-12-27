c217da5501eadd2716665a30059a49556b59f397
Yangye Zhu <yangye.zhu@wishlife.com>
2019-12-27 13:50:07 -0800

    add more tests

--- test/simple_sign/sign_test.clj
+++ test/simple_sign/sign_test.clj
@@ -1,6 +1,7 @@
 (ns simple-sign.sign-test
   (:require [clojure.test :refer :all]
-            [simple.sign :refer :all]))
+            [simple.sign :refer :all]
+            [simple.encrypt :refer :all]))
 
 (deftest test-hmac-sha1-sign
   (testing "non-empty key, non-empty string-to-sign"
@@ -9,3 +10,9 @@
   (testing "empty key, empty string-to-sign"
     ; (is (= "+9sdGxiqbAgyS31ktx+3Y3BpDh0=" (hmac-sha1-sign "" ""))) ;; IllegalArgumentException - if algorithm is null or key is null or empty.
     ))
+
+((deftest test-hmac-sha256-encrypt
+  (testing "non-empty key, non-empty string-to-encrypt"
+    (let [iv (buddy.core.nonce/random-bytes 16)]
+      (is (= "The quick brown fox jumps over the lazy dog" (hmac-sha156-decrypt "key" iv (hmac-sha256-encrypt "key" iv "The quick brown fox jumps over the lazy dog"))))))
+   ))
