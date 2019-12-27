0f0a58aac4460be6e1e55f4802f671a11c1aebaa
Yangye Zhu <yangye.zhu@wishlife.com>
2019-12-27 13:49:53 -0800

    add encrypt decrypt functions

---
+++ src/simple/encrypt.clj
@@ -0,0 +1,25 @@
+(ns simple.encrypt
+  (:require [buddy.core.hash :as hash]
+            [buddy.core.codecs :as codecs]
+            [buddy.core.nonce :as nonce]
+            [buddy.core.crypto :as crypto]
+            [cheshire.core :as json])
+  )
+
+(defn hmac-sha256-encrypt
+  [^String key
+   ^bytes iv
+   ^String string-to-encrypt]
+  (let [original-text (codecs/to-bytes string-to-encrypt)
+        key-bytes (hash/sha256 key)]
+    (crypto/encrypt original-text key-bytes iv
+                    {:algorithm :aes128-cbc-hmac-sha256})))
+
+(defn hmac-sha156-decrypt
+  [^String key
+   ^bytes iv
+   ^bytes encrypted-data]
+  (let [key-bytes (hash/sha256 key)]
+    (-> (crypto/decrypt encrypted-data key-bytes iv
+                        {:algorithm :aes128-cbc-hmac-sha256})
+        (codecs/bytes->str))))
