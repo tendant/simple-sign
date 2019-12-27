ba99e903280e9c9d92b6af38775957e54e244905
Yangye Zhu <yangye.zhu@wishlife.com>
2019-12-27 11:28:43 -0800

    update buddy

--- project.clj
+++ project.clj
@@ -7,5 +7,5 @@
                  [org.bouncycastle/bcpkix-jdk15on "1.58"]
                  [cheshire "5.8.0"]
                  [org.clojure/data.codec "0.1.1"]
-                 [buddy/buddy-core "1.4.0"]
+                 [buddy "2.0.0"]
                  [proto-repl "0.3.1"]])
