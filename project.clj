68ef14919c7600c450988c6d16ef42dddb006f57
Yangye Zhu <yangye.zhu@wishlife.com>
2019-12-27 17:49:09 -0800

    update BC

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.bouncycastle/bcpkix-jdk15on "1.58"]
+                 [org.bouncycastle/bcpkix-jdk15on "1.62"]
                  [cheshire "5.8.0"]
                  [org.clojure/data.codec "0.1.1"]
                  [buddy "2.0.0"]
